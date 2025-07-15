0419c81ea07c5322a6480deadbb620a149936d90
Maxop100 <manishkumarmahto434@gmail.com>
2025-07-15 17:46:49 +0530

    redirect option in form

--- app/(users)/contact/contact.action.jsx
+++ app/(users)/contact/contact.action.jsx
@@ -3,11 +3,13 @@
 
 
 import { db } from "@/config/db";
+import { redirect } from "next/navigation";
 
 export const contactAction = async (previousState,formData) => {
   //   const fullName = formData.get("fullName");
   //   const email = formData.get("email");
   //   const message = formData.get("message");
+  
 
   try {
     const { name, email, message } = Object.fromEntries(formData.entries());
@@ -17,8 +19,12 @@ export const contactAction = async (previousState,formData) => {
       `insert into contact_form(u_name,email,message) values (?, ? , ?)`,
       [name, email, message]
     );
-    return { success: true, message: "form submitted successfully" };
+    //return { success: true, message: "form submitted successfully" };
+    redirect("/");
+    
   } catch (error) {
+    if(error.message == "NEXT_REDIRECT") throw  error;
+
     console.log("server action: ", error);
 
     return { success: false, message: "error while submitting" };
